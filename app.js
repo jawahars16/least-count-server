10dad92701bd4a29ad0812f12c8f313021f2be45
Jawahar Suresh Babu <jawahars_16@live.in>
2020-03-22 18:58:08 +0530

    Enabled CORS

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const app = require('express')();
+const cors = require('cors')
 const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const socketio = require('socket.io');
+
+const server = app.listen(3000);
+const io = socketio().listen(server);
 const game = require('./game')(io);
+
+io.set('origins', process.env.CLIENT_ORIGIN);
+io.on('connection', game.newClient);
 
 app.get('/', function (req, res) {
     res.send('<h1>Server ready</h1>');
-});
-
-io.on('connection', game.newClient);
-
-http.listen(3000, function () {
-    console.log('listening on *:3000');
 });
